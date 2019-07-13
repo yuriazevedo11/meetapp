390dc9c56386706c132bbeee7622024ee8abf9d0
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-13 12:20:42 -0300

    NEW - Atualização de usuários, com validação dos dados de entrada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -29,7 +29,43 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ message: 'OK' });
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        )
+        .min(6),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email already registered' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = User.update(req.body);
+
+    return res.json({ id, name, email });
   }
 }
 
