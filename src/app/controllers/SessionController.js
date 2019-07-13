14ce9dfac6b6e6cb804cc87759600214ec5d90bb
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-13 11:16:32 -0300

    NEW - Autenticação do usuário, com validação dos dados de entrada

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,47 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error' });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Wrong password' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      id,
+      name,
+      email,
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e198b39492d58b5d961cecf9dc903a8a',
+  expiresIn: '7d',
+};
