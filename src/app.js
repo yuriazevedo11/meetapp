99b396c0389f1d833d52711193c1ce89b5e6cb54
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-20 18:41:40 -0300

    NEW - Acesso aos arquivos + tratamento de erros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 
 import express from 'express';
+import { resolve } from 'path';
+import 'express-async-errors';
+
 import routes from './routes';
 
 import './database';
@@ -11,14 +14,26 @@ class App {
 
     this.middlewares();
     this.routes();
+
+    this.exceptionHandler();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'temp', 'uploads'))
+    );
   }
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use((err, req, res, next) => {
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
